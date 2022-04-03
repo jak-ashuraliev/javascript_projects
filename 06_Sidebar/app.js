4f7f1fce584d8990513e4779671509e956307b0a
Jak Ashuraliev <ashuraliev@gmail.com>
2022-04-03 15:36:20 -0700

    Add functionality to the sidebar

--- 06_Sidebar/app.js
+++ 06_Sidebar/app.js
@@ -1,1 +1,21 @@
-console.log('JS Working!!!');
+const sidebarToggle = document.querySelector('.sidebar-toggle')
+const sidebar = document.querySelector('.sidebar');
+const btnClose = document.querySelector('.btn-close');
+
+// show sidebar
+sidebarToggle.addEventListener('click', function() {
+    
+    sidebar.classList.toggle('show-sidebar');
+    
+    // if (sidebar.classList.contains('show-sidebar')) {
+    //     sidebar.classList.remove('show-sidebar');
+    // } else {
+    //     sidebar.classList.add('show-sidebar');
+    // }
+    
+});
+
+// close sidebar
+btnClose.addEventListener('click', function() {
+    sidebar.classList.remove('show-sidebar');
+});
