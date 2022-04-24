a321acb0d91a4331ad81230951073f28e44a140e
Jak Ashuraliev <ashuraliev@gmail.com>
2022-04-24 09:38:39 -0700

    Setup html structure and local data menu items with images

--- 09_FoodMenu/app.js
+++ 09_FoodMenu/app.js
@@ -1,1 +1,40 @@
-console.log('JavaScript is working!')
+// menu items 
+const menu = [
+    {
+        id: 1,
+        title: "Smothered Chicken Sandwich",
+        category: "burgers",
+        price: 15.99,
+        img: "./images/menu-item-burger.png",
+        description: "Grilled chicken breast with sautéed onions, green peppers and mushrooms topped with provolone cheese served on a toasted brioche bun.",
+        calories: 1121
+    },
+    {
+        id: 2,
+        title: "Philly Cheesesteak Sandwich",
+        category: "sandwiches",
+        price: 12.29,
+        img: "./images/menu-item-sandwich.png",
+        description: "Shaved steak sautéed with onions, peppers and mushrooms topped with provolone cheese served on a toasted hoagie bun.",
+        calories: 1241
+    },
+    {
+        id: 3,
+        title: "Chicken Garden Salad",
+        category: "salads",
+        price: 14.29,
+        img: "./images/menu-item-salad.png",
+        description: "Romaine Lettuce with diced tomatoes, cucumbers, cheddar and Monterey cheese croutons and your choice of dressing topped with grilled or fried chicken strips.",
+        calories: 929
+    },
+    {
+        id: 4,
+        title: "Breaded Wings",
+        category: "salads",
+        price: 16.49,
+        img: "./images/menu-item-wings.png",
+        description: "Breaded wings with your choice of sauce and dressing. Served 10 pc",
+        calories: 1360
+    }
+];
+
